4160f82cbbdf1506ce7f62d0dc86fe8e7498b432
Bruno Henrique Crema <brunohcrema@gmail.com>
2022-11-30 09:30:45 -0300

    Criação da conexão com o banco de dados, e criação das entidades

---
+++ src/main/java/med/voll/api/controller/Medico.java
@@ -0,0 +1,29 @@
+package med.voll.api.controller;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.Especialidade;
+
+@Table(name = "medicos")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,21 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
